badf2efa93451ef6c6b671ad4afd8be8026d3300
PedrixzZDev <pedrixzz6@gmail.com>
2024-10-26 09:56:26 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
     id("fabric-loom") version ("1.7.2") apply (false)
 }
 
-val MINECRAFT_VERSION by extra { "1.21.1" }
+val MINECRAFT_VERSION by extra { "1.21.2" }
 val NEOFORGE_VERSION by extra { "21.1.23" }
-val FABRIC_LOADER_VERSION by extra { "0.16.2" }
-val FABRIC_API_VERSION by extra { "0.102.1+1.21.1" }
+val FABRIC_LOADER_VERSION by extra { "0.16.5" }
+val FABRIC_API_VERSION by extra { "0.106.1+1.21.2" }
 
 // This value can be set to null to disable Parchment.
 val PARCHMENT_VERSION by extra { null }
@@ -15,7 +15,7 @@ val PARCHMENT_VERSION by extra { null }
 val MAVEN_GROUP by extra { "me.flashyreese.mods" }
 val ARCHIVE_NAME by extra { "sodium-extra" }
 val MOD_VERSION by extra { "0.6.0-beta.3" }
-val SODIUM_VERSION by extra { "mc1.21-0.6.0-beta.2" }
+val SODIUM_VERSION by extra { "mc1.21-0.6.0-beta.3" }
 
 allprojects {
     apply(plugin = "java")
